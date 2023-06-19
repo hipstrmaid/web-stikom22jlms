07cf85d170d2263a46962c414f44c1b712415123
ray <rizkyray01@gmail.com>
2023-06-19 22:46:10 +0800

    tambah page matkul-belajar

---
+++ resources/views/frontend/pages/matkul-belajar.blade.php
@@ -0,0 +1,32 @@
+@extends('frontend.app')
+@section('content')
+    <div class="grid grid-cols-1 lg:grid-cols-8 mb-4">
+        <div class="col-span-6">
+            <article>
+                <p class="text-2xl text-grey dark:text-white font-bold mb-4">Pemrograman Berbasis Objek</p>
+                <p class="text-sm text-grey dark:text-white">Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                    Temporibus
+                    voluptates,
+                    repudiandae porro amet
+                    veritatis cum mollitia ea obcaecati! Neque, quo.</p>
+                <p class="text-sm text-grey dark:text-white">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi
+                    voluptates sapiente assumenda ipsa alias aperiam reiciendis quibusdam debitis, amet iste eligendi
+                    est
+                    odio maiores at sed ex veritatis corrupti sit delectus iusto tempora esse dolore ad quae? Quisquam
+                    pariatur totam ipsum illo quae eligendi repellendus minima quam, sequi expedita similique reiciendis
+                    molestias incidunt! Repellat cupiditate ratione quasi doloremque saepe fuga odio? Laboriosam
+                    reprehenderit at quibusdam impedit, alias tenetur placeat aperiam sed quo quidem tempore libero
+                    illum
+                    voluptate, rem nesciunt nihil. </p>
+            </article>
+
+        </div>
+        <div class="col-span-2">
+            <div class="content">
+                <div class="bg-white border border-gray-200 rounded-t shadow dark:border-gray-900 dark:bg-gray-800">
+                    <h5 class="text-gray-900 dark:text-white py-2 px-4 font-bold">Detail Mata Kuliah</h5>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
