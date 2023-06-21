11dcd3a49236c70fc9df4a7f4037a63c4f57222e
ray <rizkyray01@gmail.com>
2023-06-21 10:49:27 +0800

    minor fix belajar page

--- resources/views/frontend/includes/sidebar.blade.php
+++ resources/views/frontend/includes/sidebar.blade.php
@@ -43,8 +43,8 @@
                         <ul id="dropdown-pages" class="hidden py-2">
                             <li class="flex items-center">
                                 <a href="/matkul/matkul-saya"
-                                    class="p-2 pl-11 w-full text-base font-medium text-gray-900 rounded-lg transition duration-75 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"><i
-                                        class="fa-solid fa-book-bookmark"></i> Mata Kuliah Saya</a>
+                                    class="p-2 pl-11 w-full text-base font-medium text-gray-900 rounded-lg transition duration-75 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">Mata
+                                    Kuliah Saya</a>
                             </li>
                             <li>
                                 <a href="#"
