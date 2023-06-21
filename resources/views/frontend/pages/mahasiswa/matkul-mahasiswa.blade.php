465d6eaa35698483beec9adb782b00d0789ce8a5
ray <rizkyray01@gmail.com>
2023-06-21 14:50:42 +0800

    minor fix mahasiswa-matkul page

--- resources/views/frontend/pages/mahasiswa/matkul-mahasiswa.blade.php
+++ resources/views/frontend/pages/mahasiswa/matkul-mahasiswa.blade.php
@@ -54,6 +54,16 @@
                     <span
                         class="bg-blue-100 text-blue-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-blue-900 dark:text-blue-300">SISTEM
                         INFORMASI</span>
+                    {{-- <span
+                        class="bg-yellow-100 text-yellow-800 text-xs font-medium inline-flex items-center px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-yellow-400 border border-yellow-400">
+                        <svg aria-hidden="true" class="w-3 h-3 mr-1" fill="currentColor" viewBox="0 0 20 20"
+                            xmlns="http://www.w3.org/2000/svg">
+                            <path fill-rule="evenodd"
+                                d="M10 18a8 8 0 100-16 8 8 0 000 16zm1-12a1 1 0 10-2 0v4a1 1 0 00.293.707l2.828 2.829a1 1 0 101.415-1.415L11 9.586V6z"
+                                clip-rule="evenodd"></path>
+                        </svg>
+                        Senin, 14:55
+                    </span> --}}
                     <a href="#">
                         <h5 class="my-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Pemrogramana
                             Berbasis Objek</h5>
