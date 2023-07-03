ebab0bb99ef1c21f8cc7a8725fb08c4a58ade958
ray <rizkyray01@gmail.com>
2023-07-03 20:45:11 +0800

    buat role migration

--- database/migrations/2023_07_03_124316_create_roles_table.php
+++ database/migrations/2023_07_03_124316_create_roles_table.php
@@ -11,14 +11,10 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('roles', function (Blueprint $table) {
             $table->id();
-            $table->string('username')->unique();
-            $table->string('password');
-            $table->rememberToken();
-            $table->unsignedBigInteger('role_id'); // Updated data type to unsigned big integer
+            $table->string('name');
             $table->timestamps();
-            $table->foreign('role_id')->references('id')->on('roles');
         });
     }
 
@@ -27,6 +23,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('roles');
     }
 };
