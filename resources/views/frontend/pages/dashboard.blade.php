c27dbc93d65d258c741e0c61421baa321e4daf8e
ray <rizkyray01@gmail.com>
2023-07-06 15:08:18 +0800

    fix accordion di list matkul

--- resources/views/frontend/pages/dashboard.blade.php
+++ resources/views/frontend/pages/dashboard.blade.php
@@ -2,30 +2,27 @@
 @section('content')
     <x-content-title>Dashboard</x-content-title>
     <x-breadcrumbs></x-breadcrumbs>
-    <div class="list-matkul flex flex-col gap-3">
 
-
-        @foreach ($matkuls as $semesterId => $semesterMatkuls)
-            <div id="accordion-flush" class="" data-accordion="collapse"
-                data-active-classes="dark:bg-gray-900 text-gray-900 dark:text-white"
-                data-inactive-classes="text-gray-500 dark:text-gray-400">
-                <h2 id="accordion-flush-heading-{{ $semesterId }}">
-                    <button type="button"
-                        class="flex px-5 items-center justify-between w-full py-5 font-medium text-left text-gray-500 border border-gray-300 dark:border-gray-700 dark:text-gray-400"
-                        data-accordion-target="#accordion-flush-body-{{ $semesterId }}" aria-expanded="false"
-                        aria-controls="accordion-flush-body-1">
-                        <h2>Semester {{ $semesterId }}</h2>
-                        <svg data-accordion-icon class="w-3 h-3 rotate-180 shrink-0" aria-hidden="true"
-                            xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
-                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                d="M9 5 5 1 1 5" />
-                        </svg>
-                    </button>
-                </h2>
+    <div class="list-matkul">
+        <div id="accordion-collapse" class="flex flex-col gap-3" data-accordion="collapse">
+            @foreach ($matkuls as $semesterId => $semesterMatkuls)
                 @foreach ($semesterMatkuls as $matkul)
-                    <div id="accordion-flush-body-{{ $semesterId }}" class="hidden"
-                        aria-labelledby="accordion-flush-heading-1">
-                        <div class="py-5 dark:border-gray-700">
+                    <h2 id="accordion-collapse-heading-{{ $semesterId }}">
+                        <button type="button"
+                            class="flex items-center justify-between w-full p-5 font-medium text-left text-gray-500 border border-b-0 border-gray-200 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-800 dark:border-gray-700 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-800"
+                            data-accordion-target="#accordion-collapse-body-{{ $semesterId }}"
+                            aria-controls="accordion-collapse-body-{{ $semesterId }}">
+                            <span>Mata Kuliah: Semester {{ $semesterId }}</span>
+                            <svg data-accordion-icon class="w-3 h-3 rotate-180 shrink-0" aria-hidden="true"
+                                xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
+                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                    d="M9 5 5 1 1 5" />
+                            </svg>
+                        </button>
+                    </h2>
+                    <div id="accordion-collapse-body-{{ $semesterId }}" class="hidden"
+                        aria-labelledby="accordion-collapse-heading-{{ $semesterId }}">
+                        <div class="p-5 border border-b-0 border-gray-200 dark:border-gray-700 dark:bg-gray-900">
                             <div class="row">
                                 <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 h-46 mb-4">
                                     <div
@@ -45,7 +42,6 @@
                                             <p class="mb-3 font-xs text-gray-700 dark:text-gray-400">
                                                 {{ $matkul->excerpt() }}
                                             </p>
-
 
                                             <div class="w-full bg-gray-200 rounded-full h-2.5 mb-2.5 dark:bg-gray-700">
                                                 <div class="bg-blue-600 h-2.5 rounded-full" style="width: 45%"></div>
@@ -74,7 +70,7 @@
                         </div>
                     </div>
                 @endforeach
-            </div>
-        @endforeach
+            @endforeach
+        </div>
     </div>
 @endsection
