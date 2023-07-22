c5f86c8b2eb9f46a5c398e6504d5184a30f3eef0
ray <rizkyray01@gmail.com>
2023-07-22 21:00:28 +0800

    install breadcrumbs

--- resources/views/includes/sidebar.blade.php
+++ resources/views/includes/sidebar.blade.php
@@ -48,6 +48,42 @@
                             <li>
                                 <button type="button"
                                     class="flex items-center p-2 w-full text-base font-medium text-gray-900 rounded-lg transition duration-75 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
+                                    aria-controls="dropdown-pages-3" data-collapse-toggle="dropdown-pages-3">
+                                    <i class="fas fa-users"></i>
+                                    <span class="flex-1 ml-3 text-left whitespace-nowrap">Data User</span>
+                                    <svg aria-hidden="true" class="w-6 h-6" fill="currentColor" viewBox="0 0 20 20"
+                                        xmlns="http://www.w3.org/2000/svg">
+                                        <path fill-rule="evenodd"
+                                            d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                                            clip-rule="evenodd"></path>
+                                    </svg>
+                                </button>
+                                <ul id="dropdown-pages-3" class="hidden py-2">
+                                    <li class="flex items-center pl-10">
+                                        <span class="dot"></span>
+                                        <a href="{{ route('user.index') }}"
+                                            class="p-2 w-full text-base font-medium text-gray-900 rounded-lg transition duration-75 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">User</a>
+                                    </li>
+                                    <li class="flex items-center pl-10">
+                                        <span class="dot"></span>
+                                        <a href="{{ route('user.tambahMahasiswa') }}"
+                                            class="p-2 w-full text-base font-medium text-gray-900 rounded-lg transition duration-75 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">Mahasiswa</a>
+                                    </li>
+                                    <li class="flex items-center pl-10">
+                                        <span class="dot"></span>
+                                        <a href="{{ route('user.tambahDosen') }}"
+                                            class="p-2 w-full text-base font-medium text-gray-900 rounded-lg transition duration-75 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">Dosen</a>
+                                    </li>
+                                    <li class="flex items-center pl-10">
+                                        <span class="dot"></span>
+                                        <a href="{{ route('user.tambahAdmin') }}"
+                                            class="p-2 w-full text-base font-medium text-gray-900 rounded-lg transition duration-75 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">Admin</a>
+                                    </li>
+                                </ul>
+                            </li>
+                            <li>
+                                <button type="button"
+                                    class="flex items-center p-2 w-full text-base font-medium text-gray-900 rounded-lg transition duration-75 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
                                     aria-controls="dropdown-pages-2" data-collapse-toggle="dropdown-pages-2">
                                     <i class="fas fa-book"></i>
                                     <span class="flex-1 ml-3 text-left whitespace-nowrap">Data Umum</span>
@@ -92,42 +128,6 @@
                                             class="p-2 w-full text-base font-medium text-gray-900 rounded-lg transition duration-75 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">Role</a>
                                     </li>
 
-                                </ul>
-                            </li>
-                            <li>
-                                <button type="button"
-                                    class="flex items-center p-2 w-full text-base font-medium text-gray-900 rounded-lg transition duration-75 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
-                                    aria-controls="dropdown-pages-3" data-collapse-toggle="dropdown-pages-3">
-                                    <i class="fas fa-users"></i>
-                                    <span class="flex-1 ml-3 text-left whitespace-nowrap">Data User</span>
-                                    <svg aria-hidden="true" class="w-6 h-6" fill="currentColor" viewBox="0 0 20 20"
-                                        xmlns="http://www.w3.org/2000/svg">
-                                        <path fill-rule="evenodd"
-                                            d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                                            clip-rule="evenodd"></path>
-                                    </svg>
-                                </button>
-                                <ul id="dropdown-pages-3" class="hidden py-2">
-                                    <li class="flex items-center pl-10">
-                                        <span class="dot"></span>
-                                        <a href="{{ route('user.index') }}"
-                                            class="p-2 w-full text-base font-medium text-gray-900 rounded-lg transition duration-75 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">User</a>
-                                    </li>
-                                    <li class="flex items-center pl-10">
-                                        <span class="dot"></span>
-                                        <a href="{{ route('user.tambahMahasiswa') }}"
-                                            class="p-2 w-full text-base font-medium text-gray-900 rounded-lg transition duration-75 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">Mahasiswa</a>
-                                    </li>
-                                    <li class="flex items-center pl-10">
-                                        <span class="dot"></span>
-                                        <a href="{{ route('user.tambahDosen') }}"
-                                            class="p-2 w-full text-base font-medium text-gray-900 rounded-lg transition duration-75 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">Dosen</a>
-                                    </li>
-                                    <li class="flex items-center pl-10">
-                                        <span class="dot"></span>
-                                        <a href="{{ route('user.tambahAdmin') }}"
-                                            class="p-2 w-full text-base font-medium text-gray-900 rounded-lg transition duration-75 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">Admin</a>
-                                    </li>
                                 </ul>
                             </li>
                         @endif
