930df2ec7f633416a31a4c4f34bcda96f0fdf205
ray <rizkyray01@gmail.com>
2023-08-10 19:56:38 +0800

    create: breadcrumbs section

--- resources/views/frontend/pages/dashboard.blade.php
+++ resources/views/frontend/pages/dashboard.blade.php
@@ -1,13 +1,15 @@
 @extends('layouts.app')
 @section('content')
-    <x-content-title>Dashboard</x-content-title>
-    <x-breadcrumbs></x-breadcrumbs>
+    <div class="bg-white">
+        <x-content-title>Selamat datang, {{ Auth::user()->role->nama_role }}.</x-content-title>
+        <x-breadcrumbs></x-breadcrumbs>
+    </div>
     <div class="list-matkul">
         <div id="accordion-collapse" class="flex flex-col gap-2" data-accordion="collapse">
             @foreach ($matkuls as $semesterId => $matkulGroup)
                 <h2 id="accordion-collapse-heading-{{ $semesterId }}">
                     <button type="button"
-                        class="flex items-center justify-between w-full bg-white p-3 rounded rounded-lg font-medium text-left text-gray-900 border border-gray-200 dark:border-gray-700 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-800"
+                        class="flex items-center justify-between w-full bg-white dark:bg-gray-900 p-3 rounded rounded-lg font-medium text-left text-gray-900 border border-gray-200 dark:border-gray-700 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-800"
                         data-accordion-target="#accordion-collapse-body-{{ $semesterId }}"
                         aria-controls="accordion-collapse-body-{{ $semesterId }}">
                         <span><i class="fas fa-folder mr-2"></i>Semester {{ $semesterId }}</span>
