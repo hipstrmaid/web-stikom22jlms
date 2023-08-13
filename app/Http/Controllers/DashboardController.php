e1b43a17f93553a22c7c8e69e46062ef8659322e
ray <rizkyray01@gmail.com>
2023-08-13 17:08:04 +0800

    change: redirect Auth user to /dashboard

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -10,7 +10,19 @@ use Illuminate\Support\Facades\Auth;
 class DashboardController extends Controller
 {
     /**
-     * Display a listing of the resource.
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Show the application dashboard after Authenticated.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
      */
     public function index()
     {
