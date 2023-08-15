0691592deef9d2095bcdbf6fe6d4b1800f28d460
ray <rizkyray01@gmail.com>
2023-08-15 17:12:06 +0800

    add: tambah dan view pertemuan

--- app/Http/Controllers/PertemuanController.php
+++ app/Http/Controllers/PertemuanController.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Matkul;
+use App\Models\Pertemuan;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class PertemuanController extends Controller
 {
@@ -11,7 +14,13 @@ class PertemuanController extends Controller
      */
     public function index()
     {
-        return view('frontend.pertemuan');
+        $id = Auth::user()->dosen->id;
+        $matkuls = Matkul::where('dosen_id', $id)->get();
+        //Cari matkul
+        $matkul = Matkul::with('semester', 'prodi')->first();
+
+        $pertemuan = Pertemuan::where('matkul_id', $matkuls)->get();
+        return view('frontend/pages/dosen/pertemuan/view-pertemuan', ['matkul' => $matkul], ['pertemuans' => $pertemuan]);
     }
 
     /**
@@ -28,7 +37,15 @@ class PertemuanController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $request->validate([
+            'nama_matkul' => 'required',
+            'video_url' => 'required',
+            'deskripsi' => 'required|min:100',
+            'gambar' => 'required|file',
+            'semester_id' => 'required',
+            'hari' => 'required',
+            'prodi' => 'required',
+        ]);
     }
 
     /**
--- resources/views/frontend/pages/dosen/pertemuan/tambah-pertemuan.blade.php
+++ resources/views/frontend/pages/dosen/pertemuan/tambah-pertemuan.blade.php
@@ -7,18 +7,14 @@
 
     <div class="grid grid-cols-1 mb-5">
         <ul class="flex bg-gray-200 dark:bg-gray-900">
-            <div class="dark:border-gray-900 dark:bg-gray-900">
-                <h5 class="text-sm text-gray-900 dark:text-white py-2 px-4 font-bold"><a
-                        href="/matkul/edit-matkul">Pertemuan</a></h5>
-            </div>
             <div class="bg-white border border-gray-200 border-b-0 rounded-t dark:border-gray-900 dark:bg-gray-800">
                 <h5 class="text-sm text-gray-900 dark:text-white py-2 px-4 font-bold"><a
                         href="/matkul/edit-pertemuan">Tambah</a></h5>
             </div>
-            <div class="dark:border-gray-900 dark:bg-gray-900">
+            {{-- <div class="dark:border-gray-900 dark:bg-gray-900">
                 <h5 class="text-sm text-gray-900 dark:text-white py-2 px-4 font-bold"><a
                         href="/matkul/edit-peserta">Peserta</a></h5>
-            </div>
+            </div> --}}
         </ul>
 
         <div class="bg-white text-sm border border-gray-200 border-t-0 dark:border-gray-900 dark:bg-gray-800">
@@ -27,7 +23,7 @@
                     <div class="mb-6">
                         <label class="block mb-2 text-sm font-medium text-blue-600 dark:text-white"><span>Pertemuan
                                 1 : </span>Judul pertemuan</label>
-                        <input type="email" id="email"
+                        <input type="text" id="email" name="Nama_pertemuan"
                             class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                             placeholder="cth. Pengenalan Adobe Photoshop" required>
                     </div>
@@ -35,7 +31,7 @@
                     <div class="mb-6">
 
                         <label class="block mb-2 text-sm font-medium text-blue-600 dark:text-white">Deskripsi</label>
-                        <textarea id="message" rows="4"
+                        <textarea id="message" rows="4" name=""
                             class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                             placeholder=""></textarea>
 
